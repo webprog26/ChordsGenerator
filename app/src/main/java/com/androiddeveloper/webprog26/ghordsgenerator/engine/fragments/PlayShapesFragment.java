aea8df8d29360bd9b175d75a84daf45c3340feed
webprog26@gmail.com <webprog26@gmail.com>
2017-06-02 09:12:03 +0300

    Shape start fret position showing realized

--- app/src/main/java/com/androiddeveloper/webprog26/ghordsgenerator/engine/fragments/PlayShapesFragment.java
+++ app/src/main/java/com/androiddeveloper/webprog26/ghordsgenerator/engine/fragments/PlayShapesFragment.java
@@ -4,18 +4,32 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.androiddeveloper.webprog26.ghordsgenerator.R;
 import com.androiddeveloper.webprog26.ghordsgenerator.engine.events.InitNotesWithDrawablesEvent;
 import com.androiddeveloper.webprog26.ghordsgenerator.engine.events.NotesInitializedWithDrawablesEvent;
 import com.androiddeveloper.webprog26.ghordsgenerator.engine.fretboard.Fretboard;
 import com.androiddeveloper.webprog26.ghordsgenerator.engine.fretboard.guitar_string.GuitarString;
+import com.androiddeveloper.webprog26.ghordsgenerator.engine.helpers.FretNumbersTransformHelper;
 import com.androiddeveloper.webprog26.ghordsgenerator.engine.managers.fragments_managers.PlayShapeFragmentManager;
+import com.androiddeveloper.webprog26.ghordsgenerator.engine.models.Chord;
 import com.androiddeveloper.webprog26.ghordsgenerator.engine.models.ChordShape;
 import com.androiddeveloper.webprog26.ghordsgenerator.engine.models.Note;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+
+import butterknife.BindView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by webpr on 31.05.2017.
@@ -28,6 +42,20 @@ public class PlayShapesFragment extends Fragment {
     public static final String CHORD_SHAPE_TO_PLAY = "chord_shape_to_play";
 
     private PlayShapeFragmentManager mPlayShapeFragmentManager;
+
+    @BindView(R.id.gl_fret)
+    GridLayout mGlFret;
+    @BindView(R.id.tv_fret_to_start)
+    TextView mTvFretToStart;
+    @BindViews({R.id.first_string_muted,
+            R.id.second_string_muted,
+            R.id.third_string_muted,
+            R.id.fourth_string_muted,
+            R.id.fifth_string_muted,
+            R.id.sixth_string_muted})
+    ImageView[] mStringMutedImageViews;
+
+    private Unbinder unbinder;
 
     public static PlayShapesFragment newInstance(ChordShape chordShapeToPlay){
         Bundle args = new Bundle();
@@ -45,6 +73,15 @@ public class PlayShapesFragment extends Fragment {
         EventBus.getDefault().register(this);
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.play_shapes_fragment_layout, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+        return view;
+    }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -56,7 +93,9 @@ public class PlayShapesFragment extends Fragment {
 
             if(chordShape != null){
 
-                mPlayShapeFragmentManager = new PlayShapeFragmentManager(chordShape, getActivity().getResources(), getActivity().getAssets());
+                mPlayShapeFragmentManager = new PlayShapeFragmentManager(chordShape,
+                                                                         getActivity().getResources(),
+                                                                         getActivity().getAssets());
                 mPlayShapeFragmentManager.createSoundPool();
 
                 mPlayShapeFragmentManager.initNotesWithDrawables();
@@ -74,6 +113,12 @@ public class PlayShapesFragment extends Fragment {
 
         }
         super.onPause();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 
     @Override
@@ -99,6 +144,19 @@ public class PlayShapesFragment extends Fragment {
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onNotesInitializedWithDrawablesEvent(NotesInitializedWithDrawablesEvent notesInitializedWithDrawablesEvent){
+
+        final PlayShapeFragmentManager playShapeFragmentManager = getPlayShapeFragmentManager();
+
+        if(playShapeFragmentManager != null){
+
+            final ChordShape chordShape = playShapeFragmentManager.getChordShape();
+
+            if(chordShape != null){
+
+                getTvFretToStart().setText(FretNumbersTransformHelper.getFretToStartString(chordShape.getStartFretPosition()));
+
+            }
+        }
 
         for(int i = 0; i < Fretboard.STRINGS_COUNT; i++){
 
@@ -138,6 +196,9 @@ public class PlayShapesFragment extends Fragment {
             }
 
         }
-        //Todo load sound to every note
+    }
+
+    public TextView getTvFretToStart() {
+        return mTvFretToStart;
     }
 }
