d6949604228a998c4fb8d290ef6eb56671ad55bb
webprog26@gmail.com <webprog26@gmail.com>
2017-06-08 13:08:34 +0300

    Note image click animation added

--- app/src/main/java/com/androiddeveloper/webprog26/ghordsgenerator/engine/listeners/NoteClickListener.java
+++ app/src/main/java/com/androiddeveloper/webprog26/ghordsgenerator/engine/listeners/NoteClickListener.java
@@ -1,7 +1,13 @@
 package com.androiddeveloper.webprog26.ghordsgenerator.engine.listeners;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 
 import com.androiddeveloper.webprog26.ghordsgenerator.engine.models.Note;
@@ -11,6 +17,8 @@ import com.androiddeveloper.webprog26.ghordsgenerator.engine.models.Note;
  */
 
 public class NoteClickListener implements View.OnClickListener {
+
+    private final String TAG = "NoteClickListener";
 
     private final Note note;
 
@@ -25,23 +33,45 @@ public class NoteClickListener implements View.OnClickListener {
 
         if(note != null){
 
-            Drawable noteImageDrawable;
-            if(!note.isFingerIndexVisible()){
-                noteImageDrawable = note.getNoteFingerIndexDrawable();
-                note.setFingerIndexVisible(true);
-            } else {
-                noteImageDrawable = note.getNoteTitleDrawable();
-                note.setFingerIndexVisible(false);
-            }
-
-            if(noteImageDrawable != null){
-                ((ImageView) v).setImageDrawable(noteImageDrawable);
-            }
-
+            animateChanges((ImageView) v, note);
         }
     }
 
     private Note getNote() {
         return note;
+    }
+
+    private void animateChanges(final ImageView noteImageView, final Note note){
+        ObjectAnimator fadeOutAnimator = ObjectAnimator.ofFloat(noteImageView, View.ALPHA, 1f, 0);
+        fadeOutAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+
+                Drawable noteImageDrawable;
+
+                Log.i(TAG, "isFingerIndexVisible(): " + note.isFingerIndexVisible());
+
+                if(!note.isFingerIndexVisible()){
+                    noteImageDrawable = note.getNoteFingerIndexDrawable();
+                    note.setFingerIndexVisible(true);
+                } else {
+                    noteImageDrawable = note.getNoteTitleDrawable();
+                    note.setFingerIndexVisible(false);
+                }
+
+                if(noteImageDrawable != null){
+                    noteImageView.setImageDrawable(noteImageDrawable);
+                }
+            }
+        });
+        fadeOutAnimator.setDuration(200);
+        ObjectAnimator fadeInAnimator = ObjectAnimator.ofFloat(noteImageView, View.ALPHA, 0, 1f);
+        fadeInAnimator.setDuration(200);
+
+        AnimatorSet noteAnimatorSet = new AnimatorSet();
+        noteAnimatorSet.playSequentially(fadeOutAnimator, fadeInAnimator);
+        noteAnimatorSet.setInterpolator(new LinearInterpolator());
+        noteAnimatorSet.start();
     }
 }
